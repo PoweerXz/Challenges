0349e3c47aaf65ff4ddb5b01c0b0a2c67f9c0776
poweerxz <rafaelguadencio@gmail.com>
2022-10-07 23:04:13 -0300

    Primeiro commit! projeto challengs nivel 1

---
+++ challenges-nivel1.js
@@ -0,0 +1,81 @@
+// function imparPar(verify1, verify2) {
+//   if (verify1 % 2 === 0) {
+//     console.log(verify1 + " é " + "par");
+//   } else if (verify1 % 2 !== 0) {
+//     console.log(verify1 + " é " + "impar");
+//   }
+//   if (verify2 % 2 === 0) {
+//     console.log(verify2 + " é " + "par");
+//   } else if (verify2 % 2 !== 0) {
+//     console.log(verify2 + " é " + "impar");
+//   }
+//   return imparPar
+// }
+
+// imparPar(10, 30);
+
+// function menorEmaior (number1, number2) {
+//         if (number1 > 0) {
+//             console.log(number1, "é um número positivo");
+//         } else if (number1 <= 0) {
+//             console.log(number1, "é um número negativo");
+//         } if (number2 > 0) {
+//             console.log(number2, "é um número positivo");
+//         } else if (number2 <= 0) {
+//             console.log(number2, "é um número negativo");
+//         }
+//         return menorEmaior
+// }
+
+// menorEmaior(-10, 1);
+
+//     function mult2 (num1, num2) {
+//         if(num1 % 2 === 0) {
+//             console.log(num1, 'é um multiplo de 2');
+//         } else if (num1 % 2 !== 0) {
+//         console.log(num1, 'não é um multiplo de 2');
+//     } if (num2 % 2 === 0) {
+//         console.log(num2, 'é um multiplo de 2');
+//     } else if (num2 % 2 !== 0) {
+//         console.log(num2, 'não é um multiplo de 2')
+//     }
+//     return mult2;
+// }
+
+// mult2(2,3)
+
+// function quaDrado (numeroOne) {
+//     if (numeroOne && numeroOne) {
+//         console.log('Seu resultado é', + numeroOne);
+//     }
+//     return quaDrado
+// }
+
+// quaDrado(2**2)
+
+    // function triplo (parametro1, parametro2) {
+    //     if (parametro1 && parametro2) {
+    //        let resultado = parametro1 * parametro2
+    //        return resultado
+    //     }
+    // }
+
+    // console.log(triplo(3,6))
+
+    // function antecessor (numero1) {
+    //     if (numero1 && numero1) {
+    //         let resultado = numero1 - 1
+    //         return resultado
+    //     }
+    // }
+
+    // console.log(antecessor(2))
+
+    // function sucessor (nuMero) {
+    //     if (nuMero && nuMero) {
+    //         let resultado = nuMero + 1
+    //         return resultado
+    //     }
+    // }
+
+    // console.log(sucessor(4))
