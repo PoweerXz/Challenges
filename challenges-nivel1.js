3770da7c4610e5b6deab820f62ffa294ff9c0ed5
poweerxz <rafaelguadencio@gmail.com>
2022-10-12 22:55:13 -0300

    Alterei as chamadas, conecatenando com console.log para facilitar na hora de debugar o codigo

--- challenges-nivel1.js
+++ challenges-nivel1.js
@@ -1,81 +1,96 @@
-// function imparPar(verify1, verify2) {
-//   if (verify1 % 2 === 0) {
-//     console.log(verify1 + " é " + "par");
-//   } else if (verify1 % 2 !== 0) {
-//     console.log(verify1 + " é " + "impar");
-//   }
-//   if (verify2 % 2 === 0) {
-//     console.log(verify2 + " é " + "par");
-//   } else if (verify2 % 2 !== 0) {
-//     console.log(verify2 + " é " + "impar");
-//   }
-//   return imparPar
-// }
+1.
+function imparPar(verify1, verify2) {
+  if (verify1 % 2 === 0) {
+    console.log(verify1 + " é " + "par");
+  } else if (verify1 % 2 !== 0) {
+    console.log(verify1 + " é " + "impar");
+  }
+  if (verify2 % 2 === 0) {
+    console.log(verify2 + " é " + "par");
+  } else if (verify2 % 2 !== 0) {
+    console.log(verify2 + " é " + "impar");
+  }
+  return imparPar;
+}
 
-// imparPar(10, 30);
+imparPar(10, 31);
 
-// function menorEmaior (number1, number2) {
-//         if (number1 > 0) {
-//             console.log(number1, "é um número positivo");
-//         } else if (number1 <= 0) {
-//             console.log(number1, "é um número negativo");
-//         } if (number2 > 0) {
-//             console.log(number2, "é um número positivo");
-//         } else if (number2 <= 0) {
-//             console.log(number2, "é um número negativo");
-//         }
-//         return menorEmaior
-// }
+2.
 
-// menorEmaior(-10, 1);
+function positivoNegativo(number1, number2) {
+  if (number1 > 0) {
+    console.log(number1, "é um número positivo");
+  } else if (number1 <= 0) {
+    console.log(number1, "é um número negativo");
+  }
+  if (number2 > 0) {
+    console.log(number2, "é um número positivo");
+  } else if (number2 <= 0) {
+    console.log(number2, "é um número negativo");
+  }
+  return positivoNegativo;
+}
 
-//     function mult2 (num1, num2) {
-//         if(num1 % 2 === 0) {
-//             console.log(num1, 'é um multiplo de 2');
-//         } else if (num1 % 2 !== 0) {
-//         console.log(num1, 'não é um multiplo de 2');
-//     } if (num2 % 2 === 0) {
-//         console.log(num2, 'é um multiplo de 2');
-//     } else if (num2 % 2 !== 0) {
-//         console.log(num2, 'não é um multiplo de 2')
-//     }
-//     return mult2;
-// }
+positivoNegativo(-10, 1);
 
-// mult2(2,3)
+3.
 
-// function quaDrado (numeroOne) {
-//     if (numeroOne && numeroOne) {
-//         console.log('Seu resultado é', + numeroOne);
-//     }
-//     return quaDrado
-// }
+function mult2(num1, num2) {
+  if (num1 % 2 === 0) {
+    console.log(num1, "é um multiplo de 2");
+  } else if (num1 % 2 !== 0) {
+    console.log(num1, "não é um multiplo de 2");
+  }
+  if (num2 % 2 === 0) {
+    console.log(num2, "é um multiplo de 2");
+  } else if (num2 % 2 !== 0) {
+    console.log(num2, "não é um multiplo de 2");
+  }
+  return mult2;
+}
 
-// quaDrado(2**2)
+mult2(2, 3);
 
-    // function triplo (parametro1, parametro2) {
-    //     if (parametro1 && parametro2) {
-    //        let resultado = parametro1 * parametro2
-    //        return resultado
-    //     }
-    // }
+4.
 
-    // console.log(triplo(3,6))
+function quaDrado(numeroOne) {
+  if (numeroOne && numeroOne) {
+    console.log("Seu resultado é", +numeroOne);
+  }
+  return quaDrado;
+}
 
-    // function antecessor (numero1) {
-    //     if (numero1 && numero1) {
-    //         let resultado = numero1 - 1
-    //         return resultado
-    //     }
-    // }
+quaDrado(6 ** 2);
 
-    // console.log(antecessor(2))
+5.
 
-    // function sucessor (nuMero) {
-    //     if (nuMero && nuMero) {
-    //         let resultado = nuMero + 1
-    //         return resultado
-    //     }
-    // }
+function triplo(parametro1) {
+  if (parametro1) {
+    let resultado = parametro1 
+    return resultado;
+  }
+}
 
-    // console.log(sucessor(4))
+console.log(triplo(3 * 6));
+
+6.
+
+function antecessor(numero1) {
+  if (numero1 && numero1) {
+    let resultado = numero1 - 1;
+    return resultado;
+  }
+}
+
+console.log(antecessor(2));
+
+7.
+
+function sucessor(nuMero) {
+  if (nuMero && nuMero) {
+    let resultado = nuMero + 1;
+    return resultado;
+  }
+}
+
+console.log(sucessor(4));
